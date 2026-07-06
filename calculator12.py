e071eaa184f035c693806ffc6b8b9dd95de7387c
Ritik Sharma <ritikbhardwaj1260@gmail.com>
2026-07-06 09:01:51 +0530

    Added caculator after re-evaluated it

--- calculator12.py
+++ calculator12.py
@@ -10,19 +10,7 @@ print(" PYTHON CALCULATOR ")
 ### using while loop for various user choices
 
 while True:
-###Displaying various funtions that my calculator perform
-    print("\n Choose an operation:")
-    print("0. Exit")
-    print("1. Addition(+)")
-    print("2. Substraction(-)")
-    print("3. Multiplication(*)")
-    print("4. Division(/)")
-    print("5. Modulus(%)")
-    print("6. Power(^)")
-    print("7. Floor Division(//)")
-    print("8. Square Root")
-    print("9. Factorial")
-
+   
     ##for taking user's choice
     choice = input("Enter your choice(0-9): ")
     ##Exit the calculator
@@ -70,11 +58,25 @@ while True:
     ##Factorial##
     elif choice == "9":
         num = int (input("Enter any positive number :"))
-        num = float(input("Enter any number :"))
+        
         ##Checking if non negative number##
         if num >= 0 :
             print("Result =",math.factorial(num))
         else :
             print("Factorial is not define for negative numbers")
     else :
-        print("Invalid Choice!")
+        print("Invalid Choic!")
+    
+
+# Menu print only onceC##
+print("\nChoose an operation:")
+print("0. Exit")
+print("1. Addition(+)")
+print("2. Subtraction(-)")
+print("3. Multiplication(*)")
+print("4. Division(/)")
+print("5. Modulus(%)")
+print("6. Power(^)")
+print("7. Floor Division(//)")
+print("8. Square Root")
+print("9. Factorial")
